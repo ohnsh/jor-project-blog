eeb36e49629d2bb6d22c1433fdb7d9af17452e5c
John Sherrell <dev@ohn.sh>
2025-12-06 23:39:53 -0600

    Exercise 4

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,10 +3,11 @@ import React from 'react';
 import BlogHero from '@/components/BlogHero';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { loadBlogPost as _loadBlogPost } from '@/helpers/file-helpers';
+import CodeSnippet from '@/components/CodeSnippet';
 
 import styles from './postSlug.module.css';
 
-const loadBlogPost = React.cache(_loadBlogPost)
+const loadBlogPost = React.cache(_loadBlogPost);
 
 export async function generateMetadata({ params }) {
   const { postSlug } = await params;
@@ -27,7 +28,7 @@ async function BlogPost({ params }) {
     <article className={styles.wrapper}>
       <BlogHero title={title} publishedOn={publishedOn} />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={{ pre: CodeSnippet }} />
       </div>
     </article>
   );
